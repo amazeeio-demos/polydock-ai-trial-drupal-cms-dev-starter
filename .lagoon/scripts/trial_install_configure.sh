b1fced0b4172375f1e297a50d001dd92d9bde338
Bryan Gruneberg <hello@bryangruneberg.com>
2025-04-23 22:28:45 -0600

    Scaffold

--- .lagoon/scripts/trial_install_configure.sh
+++ .lagoon/scripts/trial_install_configure.sh
@@ -16,10 +16,16 @@ else
   # Install the provider.
   echo "Installing the amazee.io AI provider"
 
-  drush recipe /app/web/recipes/ai_provider_amazeeio_recipe \
+  # Install the provider.
+  drush recipe ../recipes/ai_provider_amazeeio_recipe \
     --input=ai_provider_amazeeio_recipe.llm_host=$AI_LLM_API_URL \
-    --input=ai_provider_amazeeio_recipe.llm_api_key=$AI_LLM_API_TOKEN
-  
+    --input=ai_provider_amazeeio_recipe.llm_api_key=$AI_LLM_API_TOKEN \
+    --input=ai_provider_amazeeio_recipe.postgres_db_host=$AI_DB_HOST_NAME  \
+    --input=ai_provider_amazeeio_recipe.postgres_db_port=5432  \
+    --input=ai_provider_amazeeio_recipe.postgres_db_username=$AI_DB_USERNAME  \
+    --input=ai_provider_amazeeio_recipe.postgres_db_password=$AI_DB_PASSWORD  \
+    --input=ai_provider_amazeeio_recipe.postgres_db_default_database=$AI_DB_NAME 
+
   # Clear the cache
   echo "Rebuilding of the Drupal cache"
   drush cr
